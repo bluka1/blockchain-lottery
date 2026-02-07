9682cd254d8b8f4788e7412008adf27e4b993ae1
Luka Batarelo <luka.batarelo@thoughtcap.com>
2026-02-07 17:36:17 +0100

    feature/3-Implement-initial-frontend - add inital routing and styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
-function App() {
+import { Route, Routes } from "react-router";
+import { LayoutPage } from "./LayoutPage";
 
+function App() {
   return (
-    <div>Blockchain lottery</div>
+    <LayoutPage>
+      <Routes>
+        <Route path="/">
+          <Route index element={<h1>How it works</h1>} />
+        </Route>
+        <Route path="/history">
+          <Route index element={<h1>History</h1>} />
+        </Route>
+        <Route path="/">
+          <Route index element={<h1>Connect wallet</h1>} />
+        </Route>
+      </Routes>
+    </LayoutPage>
   )
 }
 
